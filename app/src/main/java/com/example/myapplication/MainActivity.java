0fce5f9471dd7e2d0cb3d95ba1a4c02182b50547
Adarsh <itsadarsh8@gmail.com>
2020-05-22 17:01:07 +0530

    ViewModel initialized in Main Activity

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NotesViewModel notesViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        notesViewModel= ViewModelProviders.of(this).get(NotesViewModel.class);
+        notesViewModel.getAllNotes().observe(this, new Observer<List<Notes>>() {
+            @Override
+            public void onChanged(List<Notes> notes) {
+                //update RecyclerView
+                Toast.makeText(MainActivity.this,"Updated",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
